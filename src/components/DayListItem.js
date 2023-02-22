7cea5710fc3a7a2d833c62f841e71a808a87718f
Amaal Ali <amaalalikhadija@gmail.com>
2023-02-22 13:50:36 -0500

    made interview schedular buttons, daylist and interview list

---
+++ src/components/DayListItem.js
@@ -0,0 +1,27 @@
+import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+ const {spots} = props
+  const formatSpots = () => {
+    if (spots === 1) {
+      return <span>1 spot remaining</span>;
+    } else if (spots === 0) {
+      return <span>no spots remaining</span>;
+    }
+    return (<span>{spots} spots remaining</span>);
+  };
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+ 
+  return (
+    <li className={dayClass}onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots()}</h3>
+    </li>
+  );
+  
+}
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "components/InterviewerListItem.scss";
+import classNames from "classnames";
+
+export default function InterviewerListItem(props) {
+  const {id, name, selected, avatar, setInterviewer}= props
+  const interviewers = classNames("interviewers__item", {
+    "interviewers__item-image": avatar,
+    "interviewers__item--selected": selected,
+  });
+
+ 
+  return (
+    <li className={interviewers}onClick={() => setInterviewer(id)} >
+  <img
+    className="interviewers__item-image"
+    src={avatar}
+    alt="Sylvia Palmer"
+  />
+  {selected && name}
+  
+</li>
+  );
+  
+}
